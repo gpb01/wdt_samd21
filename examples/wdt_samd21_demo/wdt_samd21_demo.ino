9ccd4fd362d4646faa66af156a93f66c04988399
Guglielmo Braguglia <guglielmo.braguglia@phoenixsea.ch>
2022-01-19 16:59:13 +0100

    First Release

---
+++ examples/wdt_samd21_demo/wdt_samd21_demo.ino
@@ -0,0 +1,33 @@
+#include "wdt_samd21.h"
+
+void setup() {
+   delay ( 500 );
+   //
+   Serial.begin ( 9600 );
+   while ( !Serial ) {
+      delay ( 100 );
+   }
+   //
+   // Initialze WDT with a 2 sec. timeout
+   wdt_init ( WDT_CONFIG_PER_2K );
+}
+
+void loop() {
+   for ( byte i = 0; i < 5; i++ ) {
+      // wait a second
+      delay ( 1000 );
+      // write on the serial port
+      Serial.print ( "Iteration " );
+      Serial.print ( i + 1 );
+      Serial.println ( " of 5" );
+      // "feed" the WDT to avoid restart
+      wdt_reset();
+   }
+   //
+   // now wait 4 seconds ... the WDT should restart the board
+   Serial.println ( "Now waiting for 4 seconds ..." );
+   delay ( 4000 );
+   //
+   Serial.println ( "You will never see this message printed" );
+   delay ( 1000 );
+}
